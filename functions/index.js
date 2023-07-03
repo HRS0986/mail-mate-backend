8026ce4c49814b7b5980d9d9d4d11c5ceb4dacba
Hirusha Fernando <heshanhfernando@gmail.com>
2023-07-03 23:53:44 +0530

    New functions

--- functions/index.js
+++ functions/index.js
@@ -5,15 +5,23 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
+function sendPushNotification(notificationData) {
+    const payload = {
+        token: "dCgfKiROQ3e7KyybACO6e8:APA91bGap8tK_4NxaaRfuWHLU1DefGAFO36yAtkoSq0QDhqI3B9qyLKKQcDm95s3SOJuYLha8zXtA-ONSvyss4b_EVcaVoxzkLLKqJh3yw5CF7Yult9USQvgShvkfDV_QsnyLwSDs3LE",
+        notification: notificationData
+    };
+    admin.messaging().send(payload).then((res) => {
+        return {success: true};
+    }).catch((error) => {
+        return {success: false};
+    });
+}
+
 exports.get_messages = functions.https.onRequest((request, response) => {
     if (request.method === "GET") {
-        functions.logger.info("Get messages", {structuredData: true});
-        return db.collection("messages").get().then((querySnapshot) => {
-            let messages = [];
-            querySnapshot.forEach((doc) => {
-                messages.push(doc.data());
-            });
-            return response.status(200).json(messages);
+        return db.collection("messages").doc("msg").get().then((querySnapshot) => {
+            let msgObject = querySnapshot.data();
+            return response.status(200).json(msgObject["Text"]);
         }).catch(err => {
             functions.logger.error(err);
             return response.status(500).send(err);
@@ -25,7 +33,6 @@ exports.get_messages = functions.https.onRequest((request, response) => {
 
 exports.get_status = functions.https.onRequest((request, response) => {
     if (request.method === "GET") {
-        functions.logger.info("Get status", {structuredData: true});
         return db.collection("status").doc("configuration").get().then((querySnapshot) => {
             return response.status(200).json(querySnapshot.data());
         }).catch(err => {
@@ -37,58 +44,14 @@ exports.get_status = functions.https.onRequest((request, response) => {
     }
 });
 
-exports.get_letter_box_password = functions.https.onRequest((request, response) => {
+exports.get_fcm_keys = functions.https.onRequest((request, response) => {
     if (request.method === "GET") {
-        functions.logger.info("Get letter box password", {structuredData: true});
-        return db.collection("security").doc("letter-pw").get().then((querySnapshot) => {
-            return response.status(200).json(querySnapshot.data());
+        return db.collection("DeviceKeys").doc("FCMKeys").get().then((querySnapshot) => {
+            const keys = querySnapshot.data()["Keys"]
+            const data = {"registration_ids": keys};
+            return response.status(200).json(data);
         }).catch(err => {
             functions.logger.error(err);
-            return response.status(500).send(err);
-        });
-    } else {
-        return response.status(405).send("Method not allowed");
-    }
-});
-
-exports.get_parcel_box_password = functions.https.onRequest((request, response) => {
-    if (request.method === "GET") {
-        functions.logger.info("Get parcel box password", {structuredData: true});
-        return db.collection("security").doc("parcel-pw").get().then((querySnapshot) => {
-            return response.status(200).json(querySnapshot.data());
-        }).catch(err => {
-            functions.logger.error(err);
-            return response.status(500).send(err);
-        });
-    } else {
-        return response.status(405).send("Method not allowed");
-    }
-});
-
-exports.update_letter_box_password = functions.https.onRequest((request, response) => {
-    if (request.method === "PATCH") {
-        functions.logger.info("Update letter box password", {structuredData: true});
-        return db.collection("security").doc("letter-pw").set({
-            Hash: request.body
-        }).then(querySnapshot => {
-            return response.status(200).json(true);
-        }).catch(err => {
-            return response.status(500).send(err);
-        });
-    } else {
-        return response.status(405).send("Method not allowed");
-    }
-});
-
-exports.update_parcel_box_password = functions.https.onRequest((request, response) => {
-    if (request.method === "PATCH") {
-        functions.logger.info("Update parcel box password", {structuredData: true});
-        return db.collection("security").doc("parcel-pw").set({
-            Hash: request.body
-        }).then(querySnapshot => {
-            return response.status(200).json(true);
-            ;
-        }).catch(err => {
             return response.status(500).send(err);
         });
     } else {
@@ -97,11 +60,15 @@ exports.update_parcel_box_password = functions.https.onRequest((request, respons
 });
 
 exports.update_letter_count = functions.https.onRequest((request, response) => {
-    if (request.method === "PATCH") {
-        functions.logger.info("Update letter count", {structuredData: true});
+    if (request.method === "PUT") {
         return db.collection("status").doc("configuration").update({
-            LetterCount: request.body
-        }).then(querySnapshot => {
+            LetterCount: request.body["LetterCount"]
+        }).then(_ => {
+            const notification = {
+                "title": "MaleMate",
+                "body": "You have new letters!"
+            };
+            sendPushNotification(notification);
             return response.status(200).json(true);
         }).catch(err => {
             return response.status(500).send(err);
@@ -109,14 +76,12 @@ exports.update_letter_count = functions.https.onRequest((request, response) => {
     } else {
         return response.status(405).send("Method not allowed");
     }
-
 });
 
 exports.update_letter_full = functions.https.onRequest((request, response) => {
-    if (request.method === "PATCH") {
-        functions.logger.info("Update letter full", {structuredData: true});
+    if (request.method === "PUT") {
         return db.collection("status").doc("configuration").update({
-            IsLetterBoxFull: request.body
+            IsLetterBoxFull: Boolean(request.body)
         }).then(querySnapshot => {
             return response.status(200).json(true);
         }).catch(err => {
@@ -128,10 +93,9 @@ exports.update_letter_full = functions.https.onRequest((request, response) => {
 });
 
 exports.update_parcel_contain_status = functions.https.onRequest((request, response) => {
-    if (request.method === "PATCH") {
-        functions.logger.info("Update parcel contain status", {structuredData: true});
+    if (request.method === "PUT") {
         return db.collection("status").doc("configuration").update({
-            IsParcelContain: request.body
+            IsParcelContain: Boolean(request.body)
         }).then(querySnapshot => {
             return response.status(200).json(true);
         }).catch(err => {
@@ -142,3 +106,43 @@ exports.update_parcel_contain_status = functions.https.onRequest((request, respo
     }
 });
 
+exports.update_parcel_locked_status = functions.https.onRequest((request, response) => {
+    if (request.method === "PUT") {
+        return db.collection("status").doc("configuration").update({
+            IsParcelBoxLocked: Boolean(request.body)
+        }).then(querySnapshot => {
+            return response.status(200).json(true);
+        }).catch(err => {
+            return response.status(500).send(err);
+        });
+    } else {
+        return response.status(405).send("Method not allowed");
+    }
+});
+
+exports.update_letter_locked_status = functions.https.onRequest((request, response) => {
+    if (request.method === "PUT") {
+        return db.collection("status").doc("configuration").update({
+            IsLetterBoxLocked: Boolean(request.body)
+        }).then(querySnapshot => {
+            return response.status(200).json(true);
+        }).catch(err => {
+            return response.status(500).send(err);
+        });
+    } else {
+        return response.status(405).send("Method not allowed");
+    }
+});
+
+exports.create_notification = functions.https.onRequest((request, response) => {
+    if (request.method === "POST") {
+        let data = request.body;
+        return db.collection("notifications").add(data).then(() => {
+            return response.status(200).json(true);
+        }).catch((err) => {
+            return response.status(500).send(err);
+        });
+    } else {
+        return response.status(405).send("Method not allowed");
+    }
+});
